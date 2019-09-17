ca96580b6785a7b13f80a99fd07134dc1d0c36bb
Cristian <rafael.romero_chavez@hotmail.com>
2019-09-17 16:42:59 -0500

    Formularios de inicio de sesion

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/bienvenido',function(){
+	return view("landing");
+});
